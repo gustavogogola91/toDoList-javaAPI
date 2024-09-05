5b4ac1a80c360b1e1e86ff516ee2641ee652ad53
gustavogogola91 <gustavogogola3@gmail.com>
2024-09-05 00:02:50 -0300

    TaskModel added

---
+++ src/main/java/com/api/toDoList/ToDoList/model/TaskModel.java
@@ -0,0 +1,33 @@
+package com.api.toDoList.ToDoList.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tb_task")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column(nullable = false, length = 50)
+    private String name;
+    @Column(nullable = false, length = 500)
+    private String description;
+    @Column(nullable = false)
+    private LocalDateTime dueDate;
+    @Column(nullable = false)
+    private LocalDateTime creationDate;
+    @Column(nullable = false)
+    private boolean completed;
+
+}
