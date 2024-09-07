e64df11266b928b32b6081ffa9821c4fee4162a1
gustavogogola91 <gustavogogola3@gmail.com>
2024-09-06 21:24:07 -0300

    get task by id added

--- src/main/java/com/api/toDoList/ToDoList/controller/TaskController.java
+++ src/main/java/com/api/toDoList/ToDoList/controller/TaskController.java
@@ -1,10 +1,13 @@
 package com.api.toDoList.ToDoList.controller;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.api.toDoList.ToDoList.model.TaskModel;
 import com.api.toDoList.ToDoList.services.TaskService;
@@ -18,5 +21,10 @@ public class TaskController {
     @GetMapping("/tasks/")
     public List<TaskModel> getAllTasks() {
         return taskService.getAllTasks();
+    }
+
+    @GetMapping("/tasks/{id}")
+    public Optional<TaskModel> getTaskById(@PathVariable final UUID id ) {
+        return taskService.getTaskById(id);
     }
 }
